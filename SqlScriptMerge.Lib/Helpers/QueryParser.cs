c9aaa843c6b8ff7819be4bcc669bef0609c989b8
Štěpán Husa <husaS@safetypro.cz>
2023-11-03 19:21:13 +0100

    Add TransactSql parser nuget.

---
+++ SqlScriptMerge.Lib/Helpers/QueryParser.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlMergeTool
+{
+    internal static class QueryParser
+    {
+        public static void tTEY()
+        {
+            TSqlParser parser = new TSql120Parser(false);
+            TSqlFragment fragment = parser.Parse(new StringReader("ALTER TABLE TableName ADD ColumnName INT"), out var errors);
+
+            foreach(var e in errors)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            AlterTableVisitor visitor = new AlterTableVisitor();
+            fragment.Accept(visitor);
+        }
+    }
+
+    public class AlterTableVisitor : TSqlFragmentVisitor
+    {
+        public override void Visit(AlterTableAlterColumnStatement node)
+        {
+            // Extract column name, data type, and other properties
+            string columnName = node.ColumnIdentifier.Value;
+            //DataTypeReference dataType = node.DataType;
+            // ... extract other properties
+
+            // Your logic to process the extracted information
+        }
+    }
+
+}
